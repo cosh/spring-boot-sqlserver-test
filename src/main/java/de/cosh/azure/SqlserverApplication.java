149c7b9ba0a78f7922293857c8b78e3ad120e384
Henning Rauch <Henning@RauchEntwicklung.biz>
2016-12-06 06:44:09 +0100

    first commit

---
+++ src/main/java/de/cosh/azure/SqlserverApplication.java
@@ -0,0 +1,12 @@
+package de.cosh.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlserverApplication.class, args);
+	}
+}
