51fd693d7b5a6ba7207c2a9f14f5f4185fb604dd
Henning Rauch <Henning@RauchEntwicklung.biz>
2016-12-06 08:29:47 +0100

    adds some basic controller, models, services and tests

---
+++ src/main/java/de/cosh/azure/model/PersonBuilder.java
@@ -0,0 +1,28 @@
+package de.cosh.azure.model;
+
+import java.util.Date;
+
+public class PersonBuilder {
+    private String name;
+    private Long salary;
+    private Date birthday;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSalary(Long salary) {
+        this.salary = salary;
+        return this;
+    }
+
+    public PersonBuilder setBirthday(Date birthday) {
+        this.birthday = birthday;
+        return this;
+    }
+
+    public Person createPerson() {
+        return new Person(name, salary, birthday);
+    }
+}
